2fd7617771cdd9c589a064706d5f7ddb76a8f176
Uzonyi Ákos <uzonyi.akos@gmail.com>
2020-03-13 19:49:49 +0100

    Draw circle line (not very accurate)

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -33,6 +33,44 @@
 //=============================================================================================
 #include "framework.h"
 
+class Circle {
+	vec2 centre;
+	float r;
+	float angleStep;
+
+	unsigned int vao, vbo;
+	int coordCount = 0;
+
+public:
+
+	Circle(vec2 centre, float r, float angleStep = 0.001f) : centre(centre), r(r), angleStep(angleStep) {}
+
+	Circle(const Circle& c) = delete;
+	Circle& operator=(const Circle& c) = delete;
+
+	void createBuffer() {
+		std::vector<vec2> coords;
+		for (float angle = 0; angle < 2 * M_PI + angleStep; angle += angleStep) {
+			vec2 c(cos(angle) * r + centre.x, sin(angle) * r + centre.y);
+			coords.push_back(c);
+			coordCount++;
+		}
+
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, coords.size() * sizeof(vec2), &coords[0], GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	}
+
+	void draw() {
+		glBindVertexArray(vao);
+		glDrawArrays(GL_LINE_STRIP, 0, coordCount);
+	}
+};
+
 // vertex shader in GLSL: It is a Raw string (C++11) since it contains new line characters
 const char * const vertexSource = R"(
 	#version 330				// Shader 3.3
@@ -60,32 +98,24 @@ const char * const fragmentSource = R"(
 )";
 
 GPUProgram gpuProgram; // vertex and fragment shaders
-unsigned int vao;	   // virtual world on the GPU
+
+Circle circle(vec2(0, 0), 1);
+Circle* clickCircle = nullptr;
 
 // Initialization, create an OpenGL context
 void onInitialization() {
 	glViewport(0, 0, windowWidth, windowHeight);
-
-	glGenVertexArrays(1, &vao);	// get 1 vao id
-	glBindVertexArray(vao);		// make it active
-
-	unsigned int vbo;		// vertex buffer object
-	glGenBuffers(1, &vbo);	// Generate 1 buffer
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	// Geometry with 24 bytes (6 floats or 3 x 2 coordinates)
-	float vertices[] = { -0.8f, -0.8f, -0.6f, 1.0f, 0.8f, -0.2f };
-	glBufferData(GL_ARRAY_BUFFER, 	// Copy to GPU target
-		sizeof(vertices),  // # bytes
-		vertices,	      	// address
-		GL_STATIC_DRAW);	// we do not change later
-
-	glEnableVertexAttribArray(0);  // AttribArray 0
-	glVertexAttribPointer(0,       // vbo -> AttribArray 0
-		2, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
-		0, NULL); 		     // stride, offset: tightly packed
-
-	// create program for the GPU
+	circle.createBuffer();
 	gpuProgram.create(vertexSource, fragmentSource, "outColor");
+}
+
+Circle* calcCircleLine(vec2 p1, vec2 p2) {
+	vec2 c;
+	c.y = -(1 + length(p1) - 2 * p1.x * (length(p2) - length(p1)) / (2 * p2.x - 2 * p1.x)) / 
+	      (-2 * p1.y - 2 * p1.x * (2 * p1.y - 2 * p2.y) / (2 * p2.x - 2 * p1.x));
+	c.x = (c.y * (2 * p1.y - 2 * p2.y) + length(p2) - length(p1)) / (2 * p2.x - 2 * p1.x);
+	float r = length(c - p1);
+	return new Circle(vec2(c.x, c.y), r);
 }
 
 // Window has become invalid: Redraw
@@ -105,8 +135,10 @@ void onDisplay() {
 	location = glGetUniformLocation(gpuProgram.getId(), "MVP");	// Get the GPU location of uniform variable MVP
 	glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);	// Load a 4x4 row-major float matrix to the specified location
 
-	glBindVertexArray(vao);  // Draw call
-	glDrawArrays(GL_TRIANGLES, 0 /*startIdx*/, 3 /*# Elements*/);
+	circle.draw();
+
+	if (clickCircle)
+		clickCircle->draw();
 
 	glutSwapBuffers(); // exchange buffers for double buffering
 }
@@ -128,11 +160,27 @@ void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the 
 	printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
 }
 
+std::vector<vec2> clicks;
+
 // Mouse click event
 void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
 	// Convert to normalized device space
 	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
 	float cY = 1.0f - 2.0f * pY / windowHeight;
+
+	if (state == GLUT_DOWN) {
+		clicks.push_back(vec2(cX, cY));
+		if (clicks.size() >= 2) {
+			if (clickCircle)
+				delete clickCircle;
+
+			clickCircle = calcCircleLine(clicks[0], clicks[1]);
+			clickCircle->createBuffer();
+			glutPostRedisplay();
+
+			clicks.clear();
+		}
+	}
 
 	const char * buttonStat;
 	switch (state) {
